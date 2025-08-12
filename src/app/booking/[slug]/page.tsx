0ae461bda8d8d84bc05c852373a3320ad538c248
rinkumahawar <rinkumahawar9@gmail.com>
2025-08-12 17:18:41 +0530

    add client component

--- src/app/booking/[slug]/page.tsx
+++ src/app/booking/[slug]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState, useEffect } from 'react';
 import { useRouter, useParams } from 'next/navigation';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
